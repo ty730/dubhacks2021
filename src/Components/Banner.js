f12a26a1af7c3137443013ee7e34b8aa51c72439
tawong1-1834360 <tawong1@uw.edu>
2021-10-23 12:51:12 -0700

    Added basic structure

---
+++ src/Components/Banner.js
@@ -0,0 +1,12 @@
+import './../App.css';
+
+function Banner() {
+
+  return (
+    <div className="Banner">
+        <h1>Dubhacks 2021</h1>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import './../App.css';
+
+function Footer() {
+
+  return (
+    <div className="Footer">
+
+    </div>
+  );
+}
+
+export default Footer;
