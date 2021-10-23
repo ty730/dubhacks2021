33bedcb9fa1f4f1e07344e77a2012b0ea2d290c1
Jonathan Beaubien <beaubj@uw.edu>
2021-10-23 16:19:40 -0700

    course routes

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -2,10 +2,29 @@
 
 const router = require('express').Router()
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 router.route('/list').get((req, res) => {
-  axios.get('https://google.com').then(response => res.status(200).json(response.data));
-})
+  axios.get('https://www.washington.edu/students/timeschd/WIN2022/cse.html')
+    .then(response => {
+      let data = [];
+      const $ = cheerio.load(response.data);
+      let titles = $('table[bgcolor=#99ccff]').each((i, elem) => {
+        let course = {};
+        $(elem).find('a').each((i, a) => {
+          if (i == 0) {
+            course.num = $(a).text();
+          } else {
+            course.name = $(a).text();
+          }
+        });
+        data.push(course);
+      });
+      res.status(200).json(data);
+    }).catch((error) => {
+      res.status(404).send('404 - Something went wrong!');
+    });
+});
 
 // router.route('/rankings').get((req, res) => {
 //   Meme.find()
