84c03ae309844783a0ff166545e4fcbeb2c0f262
cjjohnson99 <cjj9@uw.edu>
2021-10-23 17:54:40 -0700

    search bar

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,19 +1,39 @@
 import './../App.css';
-import React, { Component } from 'react';
+import React, { Component  } from 'react';
+import { FcSearch } from 'react-icons/fc'
 
 class Banner extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
     };
+    this.onInputchange = this.onInputchange.bind(this);
+  }
+  onInputchange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   render() {
     return (
       <div className="banner">
         <h1>DubHacks 2021</h1>
+      <div className='rowC'>
+        <input
+        name = "courseNumber"
+        type = "text"
+        onChange={this.onInputchange}
+        placeholder = "search for a course"
+        className = "searchBox"
+        >
+        </input>
+{console.log(this.state)}
+        <h3>
+           <FcSearch size={50} className='searchIcon'/> </h3>
+           </div>
       </div>
+      
     );
   }
 }
