0063544255e08d81c3a63ebf026a1d90225b817a
tawong1-1834360 <tawong1@uw.edu>
2021-10-30 22:31:59 -0700

    update for backend heroku

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 // Routers
 const courseRoute = require('./routes/course.route')
 app.use('/api', courseRoute)
